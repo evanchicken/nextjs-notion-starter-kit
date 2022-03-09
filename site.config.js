13cc6e70723d0a24b8524c936cbd4b3be08fbeac
chusight <80103970+chusight@users.noreply.github.com>
2022-03-09 20:56:53 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -18,8 +18,8 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  github: 'chusight',
+  linkedin: 'chusight1',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
